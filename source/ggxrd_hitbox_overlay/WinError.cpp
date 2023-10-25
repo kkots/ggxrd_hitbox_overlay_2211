f933d7954792be1c09fc7eaa55c38cebf498d1cd
okknots@gmail.com <KAKots@1cbit.ru>
2023-10-25 16:02:58 +0300

    added WinError class

---
+++ source/ggxrd_hitbox_overlay/WinError.cpp
@@ -0,0 +1,77 @@
+#include "pch.h"
+#include "WinError.h"
+#include <iostream>
+#include <tchar.h>
+
+WinError::WinError() {
+    code = GetLastError();
+}
+void WinError::moveFrom(WinError& src) noexcept {
+    message = src.message;
+    code = src.code;
+    src.message = NULL;
+    src.code = 0;
+}
+void WinError::copyFrom(const WinError& src) {
+    code = src.code;
+    if (src.message) {
+        size_t len = _tcslen(src.message);
+        message = (LPTSTR)LocalAlloc(0, (len + 1) * sizeof(TCHAR));
+        if (message) {
+            memcpy(message, src.message, (len + 1) * sizeof(TCHAR));
+        }
+        else {
+            WinError winErr;
+            std::cout << "Error in LocalAlloc:" << winErr.getMessage();
+            return;
+        }
+    }
+}
+WinError::WinError(const WinError& src) {
+    copyFrom(src);
+}
+WinError::WinError(WinError&& src) noexcept {
+    moveFrom(src);
+}
+std::string WinError::getMessage() {
+    if (!message) {
+        FormatMessage(
+            FORMAT_MESSAGE_ALLOCATE_BUFFER |
+            FORMAT_MESSAGE_FROM_SYSTEM |
+            FORMAT_MESSAGE_IGNORE_INSERTS,
+            NULL,
+            code,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            (LPTSTR)(&message),
+            0, NULL);
+    }
+
+    std::string result;
+    result.resize(_tcslen(message));
+    TCHAR* tc = message;
+    while (*tc != TEXT('\0')) {
+        result.push_back((char)*tc);
+        ++tc;
+    }
+
+    return result;
+}
+void WinError::clear() {
+    if (message) {
+        LocalFree(message);
+        message = NULL;
+    }
+}
+WinError::~WinError() {
+    clear();
+}
+WinError& WinError::operator=(const WinError& src) {
+    clear();
+    copyFrom(src);
+    return *this;
+}
+WinError& WinError::operator=(WinError&& src) noexcept {
+    clear();
+    moveFrom(src);
+    return *this;
+}
