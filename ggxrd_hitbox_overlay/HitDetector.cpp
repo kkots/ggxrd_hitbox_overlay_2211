edea588b30b2a81843797af52d2e0a76aa635dc9
okknots@gmail.com <KAKots@1cbit.ru>
2023-11-16 23:14:31 +0300

    fixed dummy still getting hit when F1 and F7 are used

--- ggxrd_hitbox_overlay/HitDetector.cpp
+++ ggxrd_hitbox_overlay/HitDetector.cpp
@@ -68,83 +68,85 @@ int HitDetector::HookHelp::determineHitTypeHook(void* defender, BOOL wasItType10
 		std::unique_lock<std::mutex> guard(hitDetector.orig_determineHitTypeMutex);
 		result = hitDetector.orig_determineHitType(this, defender, wasItType10Hitbox, param3, hpPtr);
 	}
-	if (gifMode.modDisabled || gifMode.hitboxDisplayDisabled) return result;
-	std::unique_lock<std::mutex> guard(hitDetector.mutex);
-	Entity thisEntity{ (char*)this };
-	Entity otherEntity{ (char*)defender };
-	if (result == 4) {
-		if (thisEntity.characterType() != -1
-			|| !otherEntity
-			|| (*(unsigned int*)(otherEntity + 0x10) == 0)) return result;
+	if (gifMode.modDisabled) return result;
+	if (!gifMode.hitboxDisplayDisabled) {
+		std::unique_lock<std::mutex> guard(hitDetector.mutex);
+		Entity thisEntity{ (char*)this };
+		Entity otherEntity{ (char*)defender };
+		if (result == 4) {
+			if (thisEntity.characterType() != -1
+				|| !otherEntity
+				|| (*(unsigned int*)(otherEntity + 0x10) == 0)) return result;
 
-		// "CounterGuard..."  +  "..Stand", "..Air", "..Crouch"
-		if (strncmp(otherEntity.animationName(), "CounterGuard", 12) == 0) {
-			for (auto it = hitDetector.rejections.begin(); it != hitDetector.rejections.end(); ++it) {
-				if (it->owner == otherEntity) {
-					hitDetector.rejections.erase(it);
-					break;
-				}
-			}
-
-			int posY = otherEntity.posY();
-
-			Rejection newRejection;
-			newRejection.owner = otherEntity;
-			newRejection.firstFrame = true;
-			newRejection.counter = DISPLAY_DURATION_REJECTION + REJECTION_DELAY;
-			newRejection.skipFrame = -1;
-			newRejection.activeFrame = DISPLAY_DURATION_REJECTION;
-			newRejection.left = otherEntity.posX() - rejectionDistance;
-			newRejection.right = newRejection.left + rejectionDistance * 2;
-			newRejection.top = posY + rejectionDistance;
-			newRejection.bottom = posY - rejectionDistance;
-			hitDetector.rejections.push_back(newRejection);
-		}
-	}
-
-	if ((result == 1 || result == 2)
-			&& (DISPLAY_DURATION_HITBOX_THAT_HIT || DISPLAY_DURATION_HURTBOX_THAT_GOT_HIT)) {
-		EntityState state;
-		otherEntity.getState(&state);
-		if (!state.strikeInvuln) {
-
-			if (DISPLAY_DURATION_HITBOX_THAT_HIT && thisEntity.characterType() == -1) {
-				std::vector<DrawHitboxArrayCallParams> theHitbox;
-				collectHitboxes(thisEntity, true, nullptr, &theHitbox, nullptr, nullptr);
-				if (!theHitbox.empty()) {
-					DetectedHitboxes boxes;
-					boxes.entity = thisEntity;
-					boxes.team = thisEntity.team();
-					boxes.hitboxes = theHitbox.front();
-					boxes.counter = DISPLAY_DURATION_HITBOX_THAT_HIT;
-					boxes.previousTime = thisEntity.currentAnimDuration();
-
-					hitDetector.hitboxesThatHit.push_back(boxes);
-				}
-			}
-
-			if (DISPLAY_DURATION_HURTBOX_THAT_GOT_HIT) {
-				DrawHitboxArrayCallParams theHurtbox;
-				collectHitboxes(otherEntity, true, &theHurtbox, nullptr, nullptr, nullptr);
-				DWORD oldTransparency = theHurtbox.fillColor >> 24;
-				theHurtbox.fillColor = replaceAlpha(oldTransparency == 0 ? 64 : oldTransparency, COLOR_HURTBOX_OLD);
-				theHurtbox.outlineColor = replaceAlpha(255, COLOR_HURTBOX_OLD);
-				if (theHurtbox.hitboxCount) {
-					for (auto it = hitDetector.hurtboxesThatGotHit.begin(); it != hitDetector.hurtboxesThatGotHit.end(); ++it) {
-						if (it->entity == otherEntity) {
-							hitDetector.hurtboxesThatGotHit.erase(it);
-							break;
-						}
+			// "CounterGuard..."  +  "..Stand", "..Air", "..Crouch"
+			if (strncmp(otherEntity.animationName(), "CounterGuard", 12) == 0) {
+				for (auto it = hitDetector.rejections.begin(); it != hitDetector.rejections.end(); ++it) {
+					if (it->owner == otherEntity) {
+						hitDetector.rejections.erase(it);
+						break;
 					}
+				}
 
-					DetectedHitboxes boxes;
-					boxes.entity = otherEntity;
-					boxes.team = otherEntity.team();
-					boxes.hitboxes = theHurtbox;
-					boxes.counter = DISPLAY_DURATION_HURTBOX_THAT_GOT_HIT;
-					boxes.previousTime = otherEntity.currentAnimDuration();
+				int posY = otherEntity.posY();
 
-					hitDetector.hurtboxesThatGotHit.push_back(boxes);
+				Rejection newRejection;
+				newRejection.owner = otherEntity;
+				newRejection.firstFrame = true;
+				newRejection.counter = DISPLAY_DURATION_REJECTION + REJECTION_DELAY;
+				newRejection.skipFrame = -1;
+				newRejection.activeFrame = DISPLAY_DURATION_REJECTION;
+				newRejection.left = otherEntity.posX() - rejectionDistance;
+				newRejection.right = newRejection.left + rejectionDistance * 2;
+				newRejection.top = posY + rejectionDistance;
+				newRejection.bottom = posY - rejectionDistance;
+				hitDetector.rejections.push_back(newRejection);
+			}
+		}
+
+		if ((result == 1 || result == 2)
+				&& (DISPLAY_DURATION_HITBOX_THAT_HIT || DISPLAY_DURATION_HURTBOX_THAT_GOT_HIT)) {
+			EntityState state;
+			otherEntity.getState(&state);
+			if (!state.strikeInvuln) {
+
+				if (DISPLAY_DURATION_HITBOX_THAT_HIT && thisEntity.characterType() == -1) {
+					std::vector<DrawHitboxArrayCallParams> theHitbox;
+					collectHitboxes(thisEntity, true, nullptr, &theHitbox, nullptr, nullptr);
+					if (!theHitbox.empty()) {
+						DetectedHitboxes boxes;
+						boxes.entity = thisEntity;
+						boxes.team = thisEntity.team();
+						boxes.hitboxes = theHitbox.front();
+						boxes.counter = DISPLAY_DURATION_HITBOX_THAT_HIT;
+						boxes.previousTime = thisEntity.currentAnimDuration();
+
+						hitDetector.hitboxesThatHit.push_back(boxes);
+					}
+				}
+
+				if (DISPLAY_DURATION_HURTBOX_THAT_GOT_HIT) {
+					DrawHitboxArrayCallParams theHurtbox;
+					collectHitboxes(otherEntity, true, &theHurtbox, nullptr, nullptr, nullptr);
+					DWORD oldTransparency = theHurtbox.fillColor >> 24;
+					theHurtbox.fillColor = replaceAlpha(oldTransparency == 0 ? 64 : oldTransparency, COLOR_HURTBOX_OLD);
+					theHurtbox.outlineColor = replaceAlpha(255, COLOR_HURTBOX_OLD);
+					if (theHurtbox.hitboxCount) {
+						for (auto it = hitDetector.hurtboxesThatGotHit.begin(); it != hitDetector.hurtboxesThatGotHit.end(); ++it) {
+							if (it->entity == otherEntity) {
+								hitDetector.hurtboxesThatGotHit.erase(it);
+								break;
+							}
+						}
+
+						DetectedHitboxes boxes;
+						boxes.entity = otherEntity;
+						boxes.team = otherEntity.team();
+						boxes.hitboxes = theHurtbox;
+						boxes.counter = DISPLAY_DURATION_HURTBOX_THAT_GOT_HIT;
+						boxes.previousTime = otherEntity.currentAnimDuration();
+
+						hitDetector.hurtboxesThatGotHit.push_back(boxes);
+					}
 				}
 			}
 		}
